3e1e886591e2b67942ffc36a1e4b9136b3dd35ad
Antonio Reuter <antonio.andrade0@walmart.com>
2016-02-13 19:35:15 -0200

    added bower. added javascript libs with bower.

--- src/main/java/com/company/admin/config/SecurityConfig.java
+++ src/main/java/com/company/admin/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -27,6 +28,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     auth
         .inMemoryAuthentication()
         .withUser(username).password(password).roles("USER");
+  }
+
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+    web
+        .ignoring()
+        .antMatchers("/**/*.html",
+            "/static/**", "/**/*.ico");
   }
 
   @Override
